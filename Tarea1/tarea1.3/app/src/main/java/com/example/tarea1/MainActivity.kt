9ba2d5810e99417bdfef40e3cad187baad912f1e
pCarbonero <pablo.carbonero@iesnervion.es>
2024-10-01 21:53:09 +0200

    [new] cositas

--- Tarea1/tarea1.3/app/src/main/java/com/example/tarea1/MainActivity.kt
+++ Tarea1/tarea1.3/app/src/main/java/com/example/tarea1/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.tarea1
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -12,6 +14,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val boton = findViewById < Button >(R.id.button4)
+
+        boton.setOnClickListener {
+            //Toast.makeText(this, "boton pulsado", Toast.LENGTH_SHORT).show()
+            val intent = Intent(this, SecondActivity::class.java)
+            startActivity(intent)
+        }
 
     }
 }
