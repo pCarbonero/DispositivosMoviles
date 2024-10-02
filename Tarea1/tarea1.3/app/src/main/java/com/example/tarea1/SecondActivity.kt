c84432fe9c8475bda359decfc4f4d7262dde1f92
pCarbonero <pablo.carbonero@iesnervion.es>
2024-10-02 20:18:49 +0200

    [upd] suma

--- Tarea1/tarea1.3/app/src/main/java/com/example/tarea1/SecondActivity.kt
+++ Tarea1/tarea1.3/app/src/main/java/com/example/tarea1/SecondActivity.kt
@@ -1,7 +1,13 @@
 package com.example.tarea1
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Message
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -16,5 +22,36 @@ class SecondActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val btnSuma = findViewById<Button>(R.id.btnSuma)
+
+        val builder: AlertDialog.Builder = AlertDialog.Builder(this)
+        builder
+            .setMessage("El campo debe ser un número")
+            .setTitle("Error")
+
+        val dialog: AlertDialog = builder.create()
+
+        btnSuma.setOnClickListener {
+            val num1 = findViewById<EditText>(R.id.num1)
+            val num2 = findViewById<EditText>(R.id.num2)
+            val textRes = findViewById<TextView>(R.id.textRes)
+            val res: Double
+            try{
+                val a = num1.text.toString().toDouble()
+                val b = num2.text.toString().toDouble()
+
+                res = suma(a, b)
+
+                textRes.text = res.toString()
+            }
+            catch (e: Exception){
+                dialog.show()
+             }
+        }
+    }
+
+
+    fun suma(numA: Double, numB: Double): Double {
+        return (numA + numB)
     }
 }
