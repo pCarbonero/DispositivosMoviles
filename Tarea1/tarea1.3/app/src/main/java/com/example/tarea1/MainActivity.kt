fc8f464a52342facb954d7e0cc28de4b2c77107a
pCarbonero <pablo.carbonero@iesnervion.es>
2024-10-03 22:00:41 +0200

    [upd] casi acabado

--- Tarea1/tarea1.3/app/src/main/java/com/example/tarea1/MainActivity.kt
+++ Tarea1/tarea1.3/app/src/main/java/com/example/tarea1/MainActivity.kt
@@ -3,11 +3,13 @@ package com.example.tarea1
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,11 +17,17 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val boton = findViewById < Button >(R.id.button4)
+        val user = findViewById<EditText>(R.id.userTxt)
+        val password = findViewById<EditText>(R.id.passwordTxt)
 
         boton.setOnClickListener {
-            //Toast.makeText(this, "boton pulsado", Toast.LENGTH_SHORT).show()
+            if (user.text.toString().equals("oliverSykes") && password.text.toString().equals("Sempiternal") ){
             val intent = Intent(this, SecondActivity::class.java)
             startActivity(intent)
+            }
+            else{
+                Toast.makeText(this, "Usuario o contraseña incorrectos", Toast.LENGTH_SHORT).show()
+            }
         }
 
     }
--- Tarea1/tarea1.3/app/src/main/java/com/example/tarea1/SecondActivity.kt
+++ Tarea1/tarea1.3/app/src/main/java/com/example/tarea1/SecondActivity.kt
@@ -6,11 +6,13 @@ import android.os.Message
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.shape.ShapeAppearanceModel.builder
 
 class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,36 +24,71 @@ class SecondActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        // VARIABLES PARA ALMACENAR LOS BOTONES
         val btnSuma = findViewById<Button>(R.id.btnSuma)
+        val btnResta = findViewById<Button>(R.id.btnResta)
+        val btnMulti = findViewById<Button>(R.id.btnMulti)
+        val btnDiv = findViewById<Button>(R.id.btnDiv)
 
-        val builder: AlertDialog.Builder = AlertDialog.Builder(this)
-        builder
-            .setMessage("El campo debe ser un número")
-            .setTitle("Error")
 
-        val dialog: AlertDialog = builder.create()
+        // VARIABLES PARA LOS NUMEROS Y REALIZAR LAS OPERACIONES
+        val num1 = findViewById<EditText>(R.id.num1)
+        val num2 = findViewById<EditText>(R.id.num2)
+        val textRes = findViewById<TextView>(R.id.textRes)
+        var res: Double
+        var a: Double
+        var b: Double
 
         btnSuma.setOnClickListener {
-            val num1 = findViewById<EditText>(R.id.num1)
-            val num2 = findViewById<EditText>(R.id.num2)
-            val textRes = findViewById<TextView>(R.id.textRes)
-            val res: Double
-            try{
-                val a = num1.text.toString().toDouble()
-                val b = num2.text.toString().toDouble()
-
-                res = suma(a, b)
-
+            if (!camposVacios(num1, num2)){
+                a = num1.text.toString().toDouble()
+                b = num2.text.toString().toDouble()
+                res = a + b
                 textRes.text = res.toString()
             }
-            catch (e: Exception){
-                dialog.show()
-             }
+        }// btnSuma
+
+        btnResta.setOnClickListener {
+            if (!camposVacios(num1, num2)){
+                a = num1.text.toString().toDouble()
+                b = num2.text.toString().toDouble()
+                res = a - b
+                textRes.text = res.toString()
+            }
+        }// btnResta
+
+        btnMulti.setOnClickListener {
+            if (!camposVacios(num1, num2)){
+                a = num1.text.toString().toDouble()
+                b = num2.text.toString().toDouble()
+                res = a * b
+                textRes.text = res.toString()
+            }
+        }// btnMulti
+
+        btnDiv.setOnClickListener {
+            if (!camposVacios(num1, num2)){
+                a = num1.text.toString().toDouble()
+                b = num2.text.toString().toDouble()
+
+                if (b == 0.0){
+                    Toast.makeText(this, "No se puede dividir por 0", Toast.LENGTH_SHORT).show()
+                }
+                else{
+                    res = a / b
+                    textRes.text = res.toString()
+                }
+            }
+        }// btnDiv
+    }
+
+    fun camposVacios(num1: EditText, num2:EditText): Boolean{
+        if (num1.text.isEmpty() || num2.text.isEmpty()){
+            Toast.makeText(this, "Completa todos los campos", Toast.LENGTH_SHORT).show()
+            return true
         }
+        else return false
     }
 
-
-    fun suma(numA: Double, numB: Double): Double {
-        return (numA + numB)
-    }
 }
