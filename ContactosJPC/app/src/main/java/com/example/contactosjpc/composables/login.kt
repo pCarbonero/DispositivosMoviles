85ad2b330822375a35ef6c6f229d5c02fdced23e
pCarbonero <pablo.carbonero@iesnervion.es>
2024-10-08 17:10:21 +0200

    [upd] en el centro algo

--- ContactosJPC/app/src/main/java/com/example/contactosjpc/composables/login.kt
+++ ContactosJPC/app/src/main/java/com/example/contactosjpc/composables/login.kt
@@ -1,13 +1,14 @@
 package com.example.contactosjpc.composables
 
-import android.text.Layout
-import android.text.Layout.Alignment
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Card
 import androidx.compose.material3.Text
@@ -18,27 +19,40 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.BiasAbsoluteAlignment
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
-import java.lang.reflect.Modifier
+import com.example.contactosjpc.Contacto
+import com.example.contactosjpc.R
+
 
 @Composable
 fun SimpleTextField() {
     var text by rememberSaveable { mutableStateOf("Usuario") }
     var password by remember { mutableStateOf("") }
 
-    Card(androidx.compose.ui.Modifier.fillMaxWidth()) {
-        Column {
-            TextField(
-                value = text,
-                onValueChange = { text = it },
-                label = { Text("Usuario") }
-            )
-        }
+
+    Column(Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally) {
+        TextField(
+            modifier = Modifier.padding(8.dp),
+            value = text,
+            onValueChange = { text = it },
+            label = { Text("Usuario") }
+
+        )
+        TextField(
+            modifier = Modifier.padding(8.dp),
+            value = password,
+            onValueChange = { password = it },
+            label = { Text("Contraseña") },
+            visualTransformation = PasswordVisualTransformation(),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password)
+        )
+
     }
+
 }
