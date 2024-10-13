f72dbc011aec484652e8692c75ec47ee2240201c
pCarbonero <pablo.carbonero@iesnervion.es>
2024-10-13 10:55:21 +0200

    [upd] ejContactos

--- ContactosJPC/app/src/main/java/com/example/contactosjpc/MainActivity.kt
+++ ContactosJPC/app/src/main/java/com/example/contactosjpc/MainActivity.kt
@@ -18,19 +18,3 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-
-/*
-*                 ItemList(
-                    itemContacto = listOf(Contacto("Juan","611123456", 0),
-                        Contacto("María","678456123", 1),
-                        Contacto("Raúl","644789456", 0),
-                        Contacto("Pablo","123456789", 0),
-                        Contacto("Sara","987654321", 1),
-                        Contacto("Amador","111111111", 0),
-                        Contacto("Elena","999777888", 1),
-                        Contacto("Angy","555444666", 1),
-                        Contacto("Alicia","222111333", 1),
-                        Contacto("Ana","693882147", 1)
-                    )
-                )
-* */
--- ContactosJPC/app/src/main/java/com/example/contactosjpc/composables/login.kt
+++ ContactosJPC/app/src/main/java/com/example/contactosjpc/composables/login.kt
@@ -1,16 +1,15 @@
 package com.example.contactosjpc.composables
 
+import android.content.Intent
+import android.widget.Toast
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material3.Card
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
@@ -21,31 +20,51 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight.Companion.Bold
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.contactosjpc.Contacto
+import com.example.contactosjpc.ContactosActivity
 import com.example.contactosjpc.R
+
 
 
 @Composable
 fun SimpleTextField() {
-    var text by rememberSaveable { mutableStateOf("Usuario") }
+    val context = LocalContext.current
+    var text by rememberSaveable { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
 
 
-    Column(Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally) {
+    Column(Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(R.drawable.cont),
+            contentDescription = "Foto inicio",
+            modifier = Modifier.height(300.dp).padding(top = 100.dp, end = 20.dp)
+        )
+
+        Text(
+            modifier = Modifier.padding(top = 60.dp),
+            text = "CONTACTOS",
+            color = Color(2,0, 30),
+            fontSize = 40.sp,
+            fontWeight = Bold
+        )
+
         TextField(
-            modifier = Modifier.padding(8.dp),
+            modifier = Modifier.padding(top = 140.dp),
             value = text,
             onValueChange = { text = it },
             label = { Text("Usuario") }
 
         )
         TextField(
-            modifier = Modifier.padding(8.dp),
+            modifier = Modifier.padding(top = 40.dp),
             value = password,
             onValueChange = { password = it },
             label = { Text("Contraseña") },
@@ -53,6 +72,25 @@ fun SimpleTextField() {
             keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password)
         )
 
-    }
+        Button(
+            onClick = {
+            if (text.isEmpty())
+            {
+                Toast.makeText(context, "Inserta un nombre de usuario", Toast.LENGTH_SHORT).show()
+            }
+            else{
+                val intent = Intent(context, ContactosActivity::class.java)
+                context.startActivity(intent)
+            }
+        },
+            colors = ButtonDefaults.buttonColors(Color(67,168, 255)),
+            modifier = Modifier.padding(top = 40.dp)
+            ) {
+            Text("Acceder");
+        }
 
+    }
 }
+
+
+
